a96c55a6ee07232773d97612e27dd5d6799ea3ca
Suresh <103111380+19914039@users.noreply.github.com>
2023-11-23 14:32:32 +0530

    Update modbus.c

--- src/modbus.c
+++ src/modbus.c
@@ -174,7 +174,7 @@ static int send_msg(modbus_t *ctx, uint8_t *msg, int msg_length)
     msg_length = ctx->backend->send_msg_pre(msg, msg_length);
 
     if (ctx->debug) {
-        for (i = 0; i < msg_length; i++)
+        // for (i = 0; i < msg_length; i++)
             // syslog(LOG_INFO,"[%.2X]", msg[i]);
        syslog(LOG_INFO,"msg received is %s \n", msg);
     }
