17d53e524fcc24f7c3c296f871a57da1464a560d
Suresh <103111380+19914039@users.noreply.github.com>
2023-11-23 18:16:58 +0530

    Update modbus.c

--- src/modbus.c
+++ src/modbus.c
@@ -177,7 +177,7 @@ static int send_msg(modbus_t *ctx, uint8_t *msg, int msg_length)
     if (ctx->debug) {
         // for (i = 0; i < msg_length; i++)
             // syslog(LOG_INFO,"[%.2X]", msg[i]);
-       memcpy (resp, msg, sizeof(msg))
+       memcpy (resp, msg, sizeof(msg));
        syslog(LOG_INFO,"msg received is %s \n", resp);
     }
 
