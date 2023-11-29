bda2035cb898d0eeb14446d0ee656b926b15e630
Suresh <103111380+19914039@users.noreply.github.com>
2023-11-29 15:13:09 +0530

    Update modbus.c

--- src/modbus.c
+++ src/modbus.c
@@ -175,12 +175,12 @@ static int send_msg(modbus_t *ctx, uint8_t *msg, int msg_length)
     openlog("slog", LOG_PID|LOG_CONS, LOG_USER);
     msg_length = ctx->backend->send_msg_pre(msg, msg_length);
 
-    /*if (ctx->debug) {
+    if (ctx->debug) {
         for (i = 0; i < msg_length; i++)
             // syslog(LOG_INFO,"[%.2X]", msg[i]);
-        ptr += sprintf(ptr, "[%.2X]", msg[i]);
-        syslog(LOG_INFO,"payload is %s \n", resp);
-    }*/
+        ptr += sprintf(ptr, "%.2X", msg[i]);
+        syslog(LOG_INFO, %s \n", resp);
+    }
 
     /* In recovery mode, the write command will be issued until to be
        successful! Disabled by default. */
