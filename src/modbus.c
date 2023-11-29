325dfa44b71a5a8b71fb4a88d14dd52cfc120f83
Suresh <103111380+19914039@users.noreply.github.com>
2023-11-29 15:15:24 +0530

    Update modbus.c

--- src/modbus.c
+++ src/modbus.c
@@ -179,7 +179,7 @@ static int send_msg(modbus_t *ctx, uint8_t *msg, int msg_length)
         for (i = 0; i < msg_length; i++)
             // syslog(LOG_INFO,"[%.2X]", msg[i]);
         ptr += sprintf(ptr, "%.2X", msg[i]);
-        syslog(LOG_INFO, %s \n", resp);
+        syslog(LOG_INFO, "%s \n", resp);
     }
 
     /* In recovery mode, the write command will be issued until to be
